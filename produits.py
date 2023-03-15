0b52b1551aa706a6b07f4b3af711005189a47ec7
xavier-delfini <xavier.delfini@laplateforme.io>
2023-03-15 21:02:00 +0100

    Finalisation de la methode recuperation informations

--- produits.py
+++ produits.py
@@ -1,9 +1,33 @@
-import _mysql_connector
+import connection as dbconnect
+
+
 class Produits:
-    def __init__(self,nom,description="",prix="",quantite=-1,id_categorie=-1):
-        __nom=nom
-        __description=description
-        __prix=prix
-        __quantite=quantite
-        __id_categorie=id_categorie
-    def get_Infos(self,nom):
+    def __init__(self, nom, description="", prix="", quantite=-1, id_categorie=-1):
+        self.__nom = nom
+        self.__description = description
+        self.__prix = prix
+        self.__quantite = quantite
+        self.__id_categorie = id_categorie
+        self.__cursor = dbconnect.db.cursor()
+
+    def selectQuery(self, query, values=""):
+        if values=="":
+            self.__cursor.execute(query)
+            return self.__cursor.fetchall()
+        else:
+            self.__cursor.execute(query, values)
+            return self.__cursor.fetchall()
+
+    def get_Infos(self, nom="", where=False):
+        SQL = "SELECT * FROM boutique.produit"
+        value = [nom]
+        if where!=False or nom!="":
+            SQL += " WHERE nom = %s"
+            return self.selectQuery(SQL,value)
+        else:
+            return self.selectQuery(SQL)
+
+
+a = Produits("a")
+print(a.get_Infos())
+print(a.get_Infos("Doom Eternal",True))
